2a5f68281cfcb078a3a32bd42236b51b6ba40cc5
Melanny VC <melannyvc27@gmail.com>
2025-01-23 21:05:48 -0600

    Initial commit

---
+++ src/test/java/com/tienda/Tienda/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
